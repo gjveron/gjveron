cf2981a5daacafa758234276beba5c0c11d5f54f
gjveron <gjveron@gmail.com>
2014-09-12 21:27:39 -0300

    e402py showing the current ip

--- e402.py
+++ e402.py
@@ -6,16 +6,10 @@ from google.appengine.api import users
 
 class HomeHandler(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
-        if user:
-            self.response.out.write("Hi :1", user.nickname)
-        else:
-            self.error(402)
-
-
-app = webapp2.WSGIApplication([
-    (r'/402', HomeHandler),
-], debug=True)
+		#self.error(402)
+		ip = self.request.remote_addr
+		self.response.write("402 Payment Required: " + ip)
+		self.response.set_status(402)
 
 
 def handle_402(request, response, exception):
@@ -23,8 +17,11 @@ def handle_402(request, response, exception):
     response.write("402 Payment")
     response.set_status(402)
 
-app.error_handlers[402] = handle_402
+app = webapp2.WSGIApplication([
+    (r'/402', HomeHandler),
+], debug=True)
 
+app.error_handlers[402] = handle_402
 
 #  Run the application
 def main():
