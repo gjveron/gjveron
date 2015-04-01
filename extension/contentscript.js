6a1c77b206f9e45cc1f5874410dfb7c74af992ac
clasificado <gjveron@gmail.com>
2015-04-01 01:31:02 -0300

    From Web

--- extension/contentscript.js
+++ extension/contentscript.js
@@ -1,6 +1,7 @@
 var s = document.createElement('script');
-s.src = chrome.extension.getURL('script.js');
-(document.head||document.documentElement).appendChild(s);
+s.src = "http://gjveron.appspot.com/spotfluxguard/spotfluxguard.js";//chrome.extension.getURL('script.js');
+var o = (document.head||document.documentElement)
+o.appendChild(s);
 s.onload = function() {
     s.parentNode.removeChild(s);
 };
